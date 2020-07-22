cbf828828dfa777e600b7945e0e84129a4e55caa
Mais1212 <64730510+Mais1212@users.noreply.github.com>
2020-07-22 23:17:23 +0300

    download book covers

--- main.py
+++ main.py
@@ -4,6 +4,20 @@ import lxml
 import os
 from pathvalidate import sanitize_filename
 
+BOOKS_NUMBER = 0
+HOST ="http://tululu.org/"
+
+def download_image(url, filename, folder = 'images/'):
+	correct_filename = sanitize_filename(filename)
+	response = requests.get(url)
+	
+	if not os.path.exists(folder):
+		os.makedirs(folder)
+
+	with open(os.path.join(folder + correct_filename) + '.png', 'wb') as book:
+  		book.write(response.content)
+
+
 def download_txt(url, filename, folder = 'books/'):
 	correct_filename = sanitize_filename(filename)
 	response = requests.get(url)
@@ -14,18 +28,18 @@ def download_txt(url, filename, folder = 'books/'):
 	with open(os.path.join(folder + correct_filename) + '.txt', 'w') as book:
   		book.write(response.text)
 
-books_umber = 0
 
-while books_umber < 10:
-	books_umber += 1
-	url = 'http://tululu.org/b' + str(books_umber)
-	book_url = 'http://tululu.org/txt.php?id=' + str(books_umber)
+while BOOKS_NUMBER < 10:
+	BOOKS_NUMBER += 1
+	url = 'http://tululu.org/b' + str(BOOKS_NUMBER)
+	book_url = 'http://tululu.org/txt.php?id=' + str(BOOKS_NUMBER)
 	response = requests.get(url)
-	response_book = requests.get(book_url)
-	print(response_book.url)
-	if response_book.url == "http://tululu.org/":
-		print("Error")
+	book_response = requests.get(book_url)
+	if book_response.url == HOST:
 		continue
 	soup = BeautifulSoup(response.text, 'lxml')
-	title_tag = soup.find("div", id="content").find('h1')
+	book_img_url = HOST + soup.find("div", class_ = "bookimage").find("img").get("src")
+	title_tag = soup.find("div", id = "content").find('h1')
+	print(book_img_url)
 	download_txt(book_url,title_tag.text.split("::")[0].strip())
+	download_image(book_img_url,str(BOOKS_NUMBER))
