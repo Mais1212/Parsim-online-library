fb5922980dd3a823b1de443ab6c1521367bf43b0
Mais1212 <dragon_bord@mail.ru>
2020-10-27 21:23:06 +0300

    Generators
    Заменил простые циклы на comprehensions.
    Обернул в ifmain.
    Обновил README

--- main.py
+++ main.py
@@ -1,168 +1,203 @@
 import json
 import os
 import requests
-import lxml
+import argparse
 from bs4 import BeautifulSoup
 from urllib.parse import urljoin
 from pathvalidate import sanitize_filename
-import argparse
 
-HOST ="http://tululu.org/"
+HOST = "http://tululu.org/"
 
-def download_comments(comments_tag, filename, folder = "comments/"):
-	book_name = filename.text.split("::")[0].strip()
-	selector_comment = "span.black"
 
-	correct_commentname = "(Коментарии)"+sanitize_filename(folder + book_name)
-	comments_list = []
-	for comment in comments_tag:
-		comments_list.append(comment.select_one(selector_comment).text)
+def download_comments(comments_tag, filename, folder="comments/"):
+    selector_comment = "span.black"
 
-	comments_str = " ".join(comments_list)
+    comments_list = [comment.select_one(selector_comment).text
+                     for comment in comments_tag]
 
-	return comments_list
+    return comments_list
+
 
 def download_image(url, filename, folder):
-	book_name = filename.text.split("::")[0].strip()
+    book_name = filename.text.split("::")[0].strip()
 
-	correct_imgename = os.path.join( folder + "images/" + sanitize_filename(book_name))
-	response = requests.get(url)
-	
-	if not os.path.exists(folder + "images/"):
-		os.makedirs(folder + "images/")
+    correct_imgename = os.path.join(
+        folder + "images/" + sanitize_filename(book_name))
+    response = requests.get(url)
 
-	with open(os.path.join(correct_imgename) + ".png", "wb") as book:
-  		book.write(response.content)
-  		return url
+    os.makedirs(folder + "images/", exist_ok=True)
+
+    with open(os.path.join(correct_imgename) + ".png", "wb") as book:
+        book.write(response.content)
+        return url
+
 
 def get_content(page, pages_content):
-	url_book = []
-	selector_url = "div.bookimage a"
-	for book in pages_content :
-		url_book.append(urljoin(page, str(book.select_one(selector_url)["href"])))
-	return url_book
+    selector_url = "div.bookimage a"
+    url_book = [urljoin(page, str(book.select_one(selector_url)["href"]))
+                for book in pages_content]
+
+    return url_book
+
 
 def download_txt(url, filename, folder):
-	book_name = filename.text.split("::")[0].strip()
-	book_author = filename.text.split("::")[1].strip()
+    book_name = filename.text.split("::")[0].strip()
+    book_author = filename.text.split("::")[1].strip()
 
-	correct_bookname = os.path.join( folder + "books/" + sanitize_filename(book_name))
-	
-	response = requests.get(url)
-		
-	if not os.path.exists(folder + "books/"):
-		os.makedirs(folder + "books/")
+    correct_bookname = os.path.join(
+        folder + "books/" + sanitize_filename(book_name))
 
-	with open(os.path.join(correct_bookname) + ".txt", "w", encoding='utf-8') as book:
-  		book.write(response.text)
-  		return book_name, book_author, correct_bookname
+    response = requests.get(url)
 
-def create_json(download_txt,download_comments, download_image, book_genres, folder):
-	genres = []
-	correct_bookname = os.path.join( folder + "json/" + sanitize_filename(download_txt[0]))
+    os.makedirs(folder + "books/", exist_ok=True)
 
-	for book_genre in book_genres:
-		genres.append(book_genre.text)
+    with open(
+        os.path.join(correct_bookname) + ".txt", "w", encoding='utf-8'
+    ) as book:
+        book.write(response.text)
+        return book_name, book_author, correct_bookname
 
-	book_info = {
-		"title" : str(download_txt[0]),
-		"author" : str(download_txt[1]),
-		"img_scr" : str(download_image),
-		"comments" : download_comments,
-		"book_path" : str(download_txt[2]),
-		"genres" : genres
-	}
 
-	if not os.path.exists(folder + "json/"):
-		os.makedirs(folder + "json/")
+def create_json(
+    download_txt, download_comments, download_image, book_genres, folder
+):
+    genres = [book_genre.text for book_genre in book_genres]
 
-	with open(correct_bookname + ".json", "w", encoding="utf8") as mu_file:
-  		json.dump(book_info, mu_file, ensure_ascii=False, indent=3)
+    correct_bookname = os.path.join(
+        folder + "json/" + sanitize_filename(download_txt[0]))
+
+    book_info = {
+        "title": str(download_txt[0]),
+        "author": str(download_txt[1]),
+        "img_scr": str(download_image),
+        "comments": download_comments,
+        "book_path": str(download_txt[2]),
+        "genres": genres
+    }
+
+    os.makedirs(folder + "json/", exist_ok=True)
+
+    with open(correct_bookname + ".json", "w", encoding="utf8") as mu_file:
+        json.dump(book_info, mu_file, ensure_ascii=False, indent=3)
+
 
 def createParser():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-sp", "--start_page", type= int,
-     help = "Какой страницой начать", default = 1)
 
-    parser.add_argument("-ep", "--end_page", type = int,
-     help = "Какой страницой закончить", default = 701)
+    parser.add_argument(
+        "-sp",
+        "--start_page",
+        type=int,
+        help="Какой страницой начать?",
+        default=1
+    )
 
-    parser.add_argument("-df", "--dest_folder",type = str,
-     help = "Путь к каталогу с результатами парсинга: картинкам, книгам, JSON.", default = "library/")
-    
-    parser.add_argument("-skip_i", "--skip_imgs", help = "Не скачивать картинки", action='store_true')
+    parser.add_argument(
+        "-ep",
+        "--end_page",
+        type=int,
+        help="Какой страницой закончить?",
+        default=701
+    )
 
-    parser.add_argument("-skip_t", "--skip_txt", help = "Не скачивать книги", action='store_true')
+    parser.add_argument(
+        "-df",
+        "--dest_folder",
+        type=str,
+        help="Путь к каталогу с результатами парсинга:\
+         картинкам, книгам, JSON.",
+        default="library/"
+    )
 
-    parser.add_argument("-jp", "--json_path", type = str,
-     help = "Указать свой путь к *.json файлу с результатами", default = "library/")
- 
+    parser.add_argument(
+        "-skip_i",
+        "--skip_imgs",
+        help="Не скачивать картинки",
+        action='store_true'
+    )
+
+    parser.add_argument(
+        "-skip_t",
+        "--skip_txt",
+        help="Не скачивать книги",
+        action='store_true'
+    )
+
+    parser.add_argument(
+        "-jp",
+        "--json_path",
+        type=str,
+        help="Указать свой путь к *.json файлу с результатами",
+        default="library/")
+
     return parser
 
 
-parser = createParser()
-namespace = parser.parse_args()
+def main():
+    parser = createParser()
+    namespace = parser.parse_args()
+
+    start_page = namespace.start_page
+    end_page = namespace.end_page
+    dest_folder = namespace.dest_folder
+    json_path = namespace.json_path
+    skip_imgs = namespace.skip_imgs
+    skip_txt = namespace.skip_txt
+
+    for page in range(start_page, end_page):
+        print(F"Страница под номером {str(page)} качается")
+
+        url = "http://tululu.org/l55/"
+        book_page_collection = urljoin(url, str(page))
+        response_books_collection = requests.get(book_page_collection)
+        soup_books_collection = BeautifulSoup(
+            response_books_collection.text, "lxml")
+
+        selector_content = "table.d_book"
+        pages_content = soup_books_collection.select(selector_content)
+        books_links = get_content(book_page_collection, pages_content)
+
+        for book_url in books_links:
+
+            response_book = requests.get(book_url)
+            soup_book = BeautifulSoup(response_book.text, "lxml")
+            print(book_url)
+            if response_book.status_code == 302:
+                continue
+
+            selector_book_img = ".bookimage img"
+            selector_title = "body div[id=content] h1"
+            selector_comments = ".texts"
+            seletor_book_genres = "span.d_book a"
+            selector_book_download = "table.d_book tr a:nth-of-type(2)"
+
+            book_img_url = HOST + \
+                str(soup_book.select_one(selector_book_img)["src"])
+            book_text_url = soup_book.select(selector_book_download)
+            title_tag = soup_book.select_one(selector_title)
+            comments_tag = soup_book.select(selector_comments)
+            book_genres = soup_book.select(seletor_book_genres)
+
+            try:
+                download_book_text_url = HOST + book_text_url[0]["href"]
+
+            except IndexError:
+                continue
+
+            comments = download_comments(comments_tag, title_tag)
+            text = ["book_name", "book_author", "correct_bookname"]
+            if skip_txt is False:
+                text = download_txt(download_book_text_url,
+                                    title_tag, dest_folder)
+            img = "http://tululu.org//images/nopic.gif"
+
+            if book_img_url == img:
+                pass
+            else:
+                if skip_imgs is False:
+                    img = download_image(book_img_url, title_tag, dest_folder)
+            create_json(text, comments, img, book_genres, json_path)
 
 
-start_page = namespace.start_page
-end_page = namespace.end_page
-dest_folder = namespace.dest_folder
-json_path = namespace.json_path
-skip_imgs = namespace.skip_imgs
-skip_txt = namespace.skip_txt
-
-
-while start_page < end_page:
-	print("Страница под номером " + str(start_page) + " качается")
-
-	url = "http://tululu.org/l55/"
-	book_page_collection = urljoin(url, str(start_page))
-	response_books_collection = requests.get(book_page_collection)
-	soup_books_collection = BeautifulSoup(response_books_collection.text, "lxml")
-
-
-	selector_content = "table.d_book"
-	pages_content = soup_books_collection.select(selector_content)
-	books_links = get_content(book_page_collection, pages_content)
-
-
-	for book_url in books_links:
-		
-		response_book = requests.get(book_url, allow_redirects = False)
-		soup_book = BeautifulSoup(response_book.text, "lxml")
-
-		if response_book.status_code == 302:
-			continue
-
-		selector_book_img = ".bookimage img"
-		selector_title = "body div[id=content] h1"
-		selector_comments = ".texts"
-		seletor_book_genres = "span.d_book a"
-		selector_book_download = "table.d_book tr a:nth-of-type(2)"
-		book_img_url = HOST + str(soup_book.select_one(selector_book_img)["src"])
-		book_text_url = soup_book.select(selector_book_download)
-		title_tag = soup_book.select_one(selector_title)
-		comments_tag = soup_book.select(selector_comments)
-		book_genres = soup_book.select(seletor_book_genres)
-
-		try:
-			download_book_text_url = HOST + book_text_url[0]["href"]
-		
-		except IndexError:
-			continue
-
-		
-
-		comments = download_comments(comments_tag, title_tag)
-		text = ["book_name", "book_author", "correct_bookname"]
-		if skip_txt == False:
-			text =  download_txt(download_book_text_url,title_tag, dest_folder)
-		img = "http://tululu.org//images/nopic.gif"
-
-		if book_img_url == img:
-			pass
-		else :
-			if skip_imgs == False:
-				img = download_image(book_img_url,title_tag, dest_folder)
-		create_json(text, comments, img, book_genres, json_path)
-	start_page += 1
+if __name__ == "__main__":
+    main()
